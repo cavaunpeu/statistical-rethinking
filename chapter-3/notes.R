dbf0cc3e2bb1c6d88120bf95a52bfa5e728b4256
William Wolf <wwolf@shopkeep.com>
2016-12-21 16:52:03 +0000

    add sections 3.2, 3.6, 3.7, 3.9, 3.11, 3.12, 3.13, 3.14, 3.15

--- chapter-3/notes.R
+++ chapter-3/notes.R
@@ -1,1 +1,61 @@
 # chapter 3
+
+## 3.2
+
+# create posterior
+p_grid <- seq(from = 0, to = 1, length.out = 1000)
+prior <- rep(x = 1, 1000)
+likelihood <- dbinom(x = 6, size = 9, prob = p_grid)
+unstandardized.posterior <- likelihood * prior
+posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+
+# sample from posterior
+samples <- sample(x = p_grid, prob = posterior, size = 1e4, replace = TRUE)
+
+# plot samples sequentially
+plot(samples)
+
+# density plot of samples
+library(rethinking)
+dens(samples)
+
+## 3.6 - intervals of defined boundaries (using just p_grid and posterior)
+
+# what is the probability that p < .5?
+sum( posterior[p_grid < .5] )
+
+## 3.7 - intervals of defined boundaries (using samples)
+
+# what is the probability that p < .5?
+mean(samples < .5)
+
+# what is the probability .5 < p < .75?
+mean(samples > .5 & samples < .75)
+
+## 3.9 - confidence intervals (using samples)
+
+# there is an 80% probability the true parameter is below what value?
+quantile(x = samples, probs = .8)
+
+# we are 80% sure the true parameter is between what values?
+quantile(x = samples, probs = c(.1, .9))
+
+## 3.11
+p_grid <- seq(from = 0, to = 1, length.out = 1000)
+prior <- rep(x = 1, 1000)
+likelihood <- dbinom(x = 3, size = 3, prob = p_grid)
+unstandardized.posterior <- likelihood * prior
+posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+samples <- sample(x = p_grid, prob = posterior, size = 1e4, replace = TRUE)
+
+## 3.12 - 50% prediction interval
+PI(samples = samples, prob = .5)
+
+## 3.13 - highest posterior density interval
+HPDI(samples = samples, prob = .5)
+
+## 3.14 - compute the MAP from the posterior
+p_grid[which.max(posterior)]
+
+## 3.15 - compute the MAP from samples of the posterior
+chainmode(chain = samples, adj = .01)
