16e1785288de270d1b7e6a2f9e7f9b78b78fa339
William Wolf <wwolf@shopkeep.com>
2016-12-26 13:37:56 +0000

    add homework problem 4H3

--- chapter-4/homework.R
+++ chapter-4/homework.R
@@ -94,3 +94,38 @@ plot(height ~ weight.standardized, data = d, col = col.alpha(rangi2, .5))
 lines(x = weight.seq, y = mu.mean)
 shade(object = mu.hpdi, lim = weight.seq)
 shade(object = height.hpdi, lim = weight.seq)
+
+## 4H3
+data(Howell1)
+d <- Howell1
+trials <- 1e5
+
+model <- map(
+  alist(
+    height ~ dnorm(mean = mu, sd = sigma),
+    mu <- alpha + beta*log(weight),
+    alpha ~ dnorm(mean = 178, sd = 100),
+    beta ~ dnorm(mean = 0, sd = 100),
+    sigma ~ dunif(min = 0, max = 50)
+  ),
+  data = d
+)
+
+# simulate mu then compute mean and hpdi
+weight.seq <- seq(from = 1, to = 70, length.out = 100)
+posterior.samples <- data.frame( mvrnorm(n = trials, mu = coef(model), Sigma = vcov(model)) )
+mu.link <- function(weight) posterior.samples$alpha + posterior.samples$beta * log(weight)
+mu <- sapply(X = weight.seq, FUN = mu.link)
+mu.mean <- apply(X = mu, MARGIN = 2, FUN = mean)
+mu.hpdi <- apply(X = mu, MARGIN = 2, FUN = HPDI, prob = .89)
+
+# simulate heights then compute hpdi
+height.link <- function(weight) rnorm(n = nrow(posterior.samples), mean = mu.link(weight), sd = posterior.samples$sigma)
+height.samples <- sapply(X = weight.seq, FUN = height.link)
+height.hpdi <- apply(X = height.samples, MARGIN = 2, FUN = HPDI, prob = .89)
+
+# plot results
+plot(height ~ weight, data = d, col = col.alpha(rangi2, .4))
+lines(x = weight.seq, y = mu.mean)
+shade(object = mu.hpdi, lim = weight.seq)
+shade(object = height.hpdi, lim = weight.seq)
