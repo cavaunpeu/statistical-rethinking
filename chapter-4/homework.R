6baa542da15eccc8ae27bb3e16d990cc004081d8
William Wolf <wwolf@shopkeep.com>
2016-12-26 11:58:44 +0000

    add homework problem 4H1

--- chapter-4/homework.R
+++ chapter-4/homework.R
@@ -1,5 +1,7 @@
 # homework
 
+library(rethinking)
+
 ## 4M1
 trials <- 1e3
 mu.prior.samples <- rnorm(n = trials, mean = 0, sd = 10)
@@ -12,3 +14,38 @@ formula.list <- alist(
   mu ~ dnorm( 0 , 10 ),
   sigma ~ dunif( 0 , 10 )
 )
+
+## 4H1
+
+# load data
+data(Howell1)
+d <- Howell1
+d$weight.centered <- (d$weight - mean(d$weight)) / sd(d$weight)
+
+# build model
+model <- map(
+  alist(
+    height ~ dnorm(mean = mu, sd = sigma),
+    mu <- alpha + beta*weight.centered,
+    alpha ~ dnorm(mean = 0, sd = 100),
+    beta ~ dnorm(mean = 0, sd = 10),
+    sigma ~ dunif(min = 0, max = 64)
+  ),
+  data = d
+)
+
+# simulate heights from model
+individual.weights <- c(46.95, 43.72, 64.78, 32.59, 54.63)
+individual.weights.centered <- (individual.weights - mean(d$weight)) / sd(d$weight)
+simulated.heights <- sim(model, data = list(weight.centered = individual.weights.centered))
+
+# summarize results
+simulated.heights.mean <- apply(X = simulated.heights, MARGIN = 2, FUN = mean)
+simulated.heights.PI <- apply(X = simulated.heights, MARGIN = 2, FUN = PI, prob = .89)
+
+# try it manually for the first individual
+posterior.samples <- extract.samples(model)
+simulated.heights.first.individual <- rnorm(n = trials, mean = posterior.samples$alpha + posterior.samples$beta*individual.weights.centered[1], sd = posterior.samples$sigma)
+simulated.heights.first.individual.mean <- mean(simulated.heights.first.individual)
+simulated.heights.first.individual.PI <- PI(samples = simulated.heights.first.individual, prob = .89)
+
