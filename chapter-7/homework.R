37d2c0155fdb5c60f08503f93f0dcf046c7519ab
William Wolf <wwolf@shopkeep.com>
2017-01-27 09:06:22 +0000

    add homework problem 7H1

--- chapter-7/homework.R
+++ chapter-7/homework.R
@@ -15,3 +15,28 @@
 # The relationship between the amount of food that a raven consumes and the presence of a wolf killing this prey cannot be entirely linear, as without the wolf the
 # raven would not eat any food at all. Once the wolf does enter the picture, though, the relationship could thereafter be a linear one.
 
+## 7H1
+library(rethinking)
+data(tulips)
+d <- tulips
+str(d)
+
+# center variables
+d$water.centered <- d$water - mean(d$water)
+d$shade.centered <- d$shade - mean(d$shade)
+d$bed.b <- ifelse(d$bed == "b", 1, 0)
+d$bed.c <- ifelse(d$bed == "c", 1, 0)
+
+# fit model with interaction term plus main effects for unique levels of `bed`
+m7H1 <- map(
+  alist(
+    blooms ~ dnorm( mu , sigma ) ,
+    mu <- alpha + beta.water*water.centered + beta.shade*shade.centered + beta.water.shade*water*shade + beta.bed.b*bed.b + beta.bed.c*bed.c,
+    c(alpha, beta.water, beta.shade, beta.water.shade, beta.bed.b, beta.bed.c) ~ dnorm( 0, 100 ),
+    sigma ~ dunif( 0 , 100 )
+  ),
+  data=d,
+  start=list(alpha=mean(d$blooms), beta.water=0, beta.shade=0, beta.water.shade=0, beta.bed.b=0, beta.bed.c=0, sigma=sd(d$blooms)), 
+  control=list(maxit=1e4)
+)
+
