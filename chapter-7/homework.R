b3dc08a0dacc33262e221b192fa9ed356cab8bab
William Wolf <wwolf@shopkeep.com>
2017-01-27 08:43:05 +0000

    add homework problems 7M1, 7M2, 7M3

---
+++ chapter-7/homework.R
@@ -0,0 +1,17 @@
+# homework
+
+## 7M1
+
+# The fact that tulips cease to grow entirely when the temperature is hot, in lieu of shrinking in bloom-size in a linear fashion, implies a non-linear model.
+# This said, we can point to an interaction between water, shade (which together were shown to be conducive to tulip growth) and temperature.
+
+## 7M2
+
+# A regression equation that would make the bloom size zero whenever the temperature is hot would include only terms interacted with an is_not_hot variable, such that when
+# this variable is 0, i.e. it is *not* is_not_hot, the bloom size would be zero.
+
+## 7M3
+
+# The relationship between the amount of food that a raven consumes and the presence of a wolf killing this prey cannot be entirely linear, as without the wolf the
+# raven would not eat any food at all. Once the wolf does enter the picture, though, the relationship could thereafter be a linear one.
+
