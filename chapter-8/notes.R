c884f88b39e5f9d554f32011c8a6756f4d4f9ea6
William Wolf <wwolf@shopkeep.com>
2017-01-27 15:48:23 +0000

    add sections 8.11, 8.17, 8.18, 8.19

--- chapter-8/notes.R
+++ chapter-8/notes.R
@@ -63,3 +63,31 @@ posterior.samples <- extract.samples(m8.1stan)
 
 ## 8.10
 pairs(m8.1)
+
+## 8.11
+show(m8.1stan)
+
+## 8.17
+y <- rnorm( 100 , mean=0 , sd=1 )
+
+## 8.18
+m8.4 <- map2stan(
+  alist(
+    y ~ dnorm( mu , sigma ) ,
+    mu <- a1 + a2 ,
+    sigma ~ dcauchy( 0 , 1 )
+  ),
+  data=list(y=y) , start=list(a1=0,a2=0,sigma=1) , chains=2 , iter=4000 , warmup=1000 )
+precis(m8.4)
+
+## 8.19
+m8.5 <- map2stan(
+  alist(
+    y ~ dnorm( mu , sigma ) ,
+    mu <- a1 + a2 ,
+    a1 ~ dnorm( 0 , 10 ) ,
+    a2 ~ dnorm( 0 , 10 ) ,
+    sigma ~ dcauchy( 0 , 1 )
+  ),
+  data=list(y=y) , start=list(a1=0,a2=0,sigma=1) , chains=2 , iter=4000 , warmup=1000 )
+precis(m8.5)
