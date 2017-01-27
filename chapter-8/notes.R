ef86fd47aa28f150cc50d5ce59a5bcf7b8213cef
William Wolf <wwolf@shopkeep.com>
2017-01-27 13:20:59 +0000

    add sections 8.4, 8.5, 8.7, 8.8, 8.10

--- chapter-8/notes.R
+++ chapter-8/notes.R
@@ -38,3 +38,28 @@ m8.1 <- map(
   data=dd )
 
 precis(m8.1)
+
+## 8.4
+dd.trim <- dd[ , c("log_gdp", "rugged", "cont_africa")]
+
+## 8.5
+m8.1stan <- map2stan(
+  alist(
+    log_gdp ~ dnorm( mu , sigma ) ,
+    mu <- a + bR*rugged + bA*cont_africa + bAR*rugged*cont_africa ,
+    a ~ dnorm(0,100),
+    bR ~ dnorm(0,10),
+    bA ~ dnorm(0,10),
+    bAR ~ dnorm(0,10),
+    sigma ~ dcauchy(0,2)
+  ), data=dd.trim )
+
+## 8.7
+m8.1stan_4chains <- map2stan(m8.1stan, chains = 4, cores = 4)
+precis(m8.1stan_4chains)
+
+## 8.8
+posterior.samples <- extract.samples(m8.1stan)
+
+## 8.10
+pairs(m8.1)
