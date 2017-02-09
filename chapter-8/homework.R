2d8d4c72dcda67b095fa33271821492c36ff0612
William Wolf <wwolf@shopkeep.com>
2017-02-09 13:42:27 +0000

    add homework problem 8M1

---
+++ chapter-8/homework.R
@@ -0,0 +1,45 @@
+# homework
+
+## 8M1
+
+library(rethinking)
+data(rugged)
+d <- rugged
+d$log_gdp <- log(d$rgdppc_2000)
+dd <- d[ complete.cases(d$rgdppc_2000) , ]
+dd.trim <- dd[ , c("log_gdp", "rugged", "cont_africa")]
+
+# put a uniform prior on sigma
+m8.1.unif <- map2stan(
+  alist(
+    log_gdp ~ dnorm( mu , sigma ) ,
+    mu <- a + bR*rugged + bA*cont_africa + bAR*rugged*cont_africa ,
+    a ~ dnorm(0, 100),
+    bR ~ dnorm(0, 10),
+    bA ~ dnorm(0, 10),
+    bAR ~ dnorm(0, 10),
+    sigma ~ dunif(0, 10)
+  ), data=dd.trim )
+
+# put an exponential prior on sigma
+m8.1.exp <- map2stan(
+  alist(
+    log_gdp ~ dnorm( mu , sigma ) ,
+    mu <- a + bR*rugged + bA*cont_africa + bAR*rugged*cont_africa ,
+    a ~ dnorm(0, 100),
+    bR ~ dnorm(0, 10),
+    bA ~ dnorm(0, 10),
+    bAR ~ dnorm(0, 10),
+    sigma ~ dexp(1)
+  ), data=dd.trim )
+
+# visualize each prior
+curve(dcauchy(x, 0, 2), from = 0, to = 10, xlab = "sigma", ylab = "Density", ylim = c(0, 1))
+curve(dunif(x, 0, 10), from = 0, to = 10, add = TRUE, col = "blue")
+curve(dexp(x, 1), from = 0, to = 10, add = TRUE, col = "red")
+
+# plot the posterior for sigma for each model
+sigma_unif <- extract.samples(m8.1.unif,pars="sigma")
+sigma_exp <- extract.samples(m8.1.exp,pars="sigma")
+dens(sigma_unif[[1]], xlab="sigma", xlim=c(0.5,1.5), col="red")
+dens(sigma_exp[[1]], add=TRUE, col="blue")
