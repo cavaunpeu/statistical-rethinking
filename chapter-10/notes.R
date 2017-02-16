c1ed5c4854ba493b92b641d4fe1dc60747581ba1
William Wolf <wwolf@shopkeep.com>
2017-02-16 12:31:33 +0000

    add sections 10.12, 10.13, 10.14, 10.16, 10.17, 10.18, 10.19

--- chapter-10/notes.R
+++ chapter-10/notes.R
@@ -72,3 +72,60 @@ for ( chimp in 1:7 )
 # superimpose posterior predictions
 lines( 1:4 , pred.p )
 shade( pred.p.PI , 1:4 )
+
+## 10.12
+
+# clean NAs from the data
+d2 <- d
+d2$recipient <- NULL
+
+# re-use map fit to get the formula
+m10.3stan <- map2stan( m10.3 , data=d2 , iter=1e4 , warmup=1000 )
+precis(m10.3stan)
+
+## 10.13
+pairs(m10.3stan)
+
+## 10.14
+m10.4 <- map2stan(
+  alist(
+    pulled_left ~ dbinom(1, p),
+    logit(p) <- a[actor] + (bp + bpC*condition)*prosoc_left,
+    a[actor] ~ dnorm(0, 10),
+    bp ~ dnorm(0, 10),
+    bpC ~ dnorm(0, 10)
+  ),
+  data = d2, chains = 2, iter = 2500, warmup = 500
+)
+
+## 10.16
+precis(m10.4, depth = 2)
+
+## 10.17
+post <- extract.samples( m10.4 )
+str(post)
+
+## 10.18
+dens( post$a[,2] )
+
+## 10.19
+chimp <- 1
+d.pred <- list(
+  pulled_left = rep( 0 , 4 ), # empty outcome
+  prosoc_left = c(0,1,0,1),   # right/left/right/left
+  condition = c(0,0,1,1),     # control/control/partner/partner
+  actor = rep(chimp,4)
+)
+link.m10.4 <- link( m10.4 , data=d.pred )
+pred.p <- apply( link.m10.4 , 2 , mean )
+pred.p.PI <- apply( link.m10.4 , 2 , PI )
+plot( 0 , 0 , type="n" , xlab="prosoc_left/condition" ,
+      ylab="proportion pulled left" , ylim=c(0,1) , xaxt="n" ,
+      xlim=c(1,4) , yaxp=c(0,1,2) )
+axis( 1 , at=1:4 , labels=c("0/0","1/0","0/1","1/1") )
+mtext( paste( "actor" , chimp ) )
+p <- by( d$pulled_left ,
+         list(d$prosoc_left,d$condition,d$actor) , mean )
+lines( 1:4 , as.vector(p[,,chimp]) , col=rangi2 , lwd=2 )
+lines( 1:4 , pred.p )
+shade( pred.p.PI , 1:4 )
