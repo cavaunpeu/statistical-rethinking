91141db2179766e3a5f2e679956bab323a850fdf
William Wolf <wwolf@shopkeep.com>
2017-02-26 16:56:26 +0000

    add sections 11.25, 11.26, 11.27, 11.28, 11.29, 11.30, 11.31

--- chapter-11/notes.R
+++ chapter-11/notes.R
@@ -147,3 +147,55 @@ m11.4 <- map(
     al ~ dnorm(0,10) ),
   data=list(y=y) )
 precis(m11.4)
+
+## 11.25
+pbar <- 0.5
+theta <- 10
+curve( dbeta2(x,pbar,theta) , from=0 , to=1 ,
+       xlab="probability" , ylab="Density" )
+
+## 11.26
+library(rethinking)
+data(UCBadmit)
+d <- UCBadmit
+
+m11.5 <- map2stan(
+  alist(
+    admit ~ dbetabinom(applications, pbar, theta),
+    logit(pbar) <- a,
+    a ~ dnorm(0,2),
+    theta ~ dexp(1)
+  ),
+  data=d,
+  constraints=list(theta="lower=0"),
+  start=list(theta=3),
+  iter=4000 , warmup=1000 , chains=2 , cores=2 )
+
+## 11.27
+precis(m11.5)
+
+## 11.28
+post <- extract.samples(m11.5)
+quantile( logistic(post$a) , c(0.025,0.5,0.975) )
+
+## 11.29
+post <- extract.samples(m11.5)
+
+# draw posterior mean beta distribution
+curve( dbeta2(x,mean(logistic(post$a)),mean(post$theta)) , from=0 , to=1 ,
+       ylab="Density" , xlab="probability admit", ylim=c(0,3) , lwd=2 )
+
+# draw 100 beta distributions sampled from posterior
+for ( i in 1:100 ) {
+  p <- logistic( post$a[i] )
+  theta <- post$theta[i]
+  curve( dbeta2(x,p,theta) , add=TRUE , col=col.alpha("black",0.2) )
+}
+
+## 11.30
+postcheck(m11.5)
+
+## 11.31
+mu <- 20
+theta <- 10
+curve( dgamma2(x,mu,theta) , from=0 , to=10 )
