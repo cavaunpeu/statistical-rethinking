f340d99a5f3681612df2d97200d9c24688b49555
William Wolf <wwolf@shopkeep.com>
2017-03-01 13:58:17 +0000

    add homework problem 11H2

--- chapter-11/homework.R
+++ chapter-11/homework.R
@@ -57,3 +57,25 @@ lines(x = prediction.data$femininity, y = lambda.mean)
 shade(object = lambda.PI, lim = prediction.data$femininity)
 lines(x = prediction.data$femininity, y = simulated.predictions.PI[1,], lty = 2)
 lines(x = prediction.data$femininity, y = simulated.predictions.PI[2,], lty = 2)
+
+## 11H2
+
+# fit model
+model.11H2 <- map2stan(
+  alist(
+    deaths ~ dgampois( mu , scale ),
+    log(mu) <- alpha + beta_femininity*femininity,
+    alpha ~ dnorm(0, 10),
+    beta_femininity ~ dnorm(0, 10),
+    scale ~ dcauchy(0, 2)
+  ),
+  data=d,
+  constraints=list(scale="lower=0"),
+  start=list(scale=2) )
+
+# inspect estimates
+precis(model.11H2)
+
+# why did the association between `beta_femininity` and `deaths` diminish in strength?
+# I assume this is because a much larger range of values of `(alpha, beta_femininity)` can produce, through the Gamma distribution, the same expected
+# values `lambda` via the variance in the Gamma process itself
