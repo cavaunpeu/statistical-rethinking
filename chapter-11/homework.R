a51f1deb5a0dd7d4291208614c8f50a70adf6342
William Wolf <wwolf@shopkeep.com>
2017-03-01 14:45:52 +0000

    add homework problem 11H3

--- chapter-11/homework.R
+++ chapter-11/homework.R
@@ -78,4 +78,74 @@ precis(model.11H2)
 
 # why did the association between `beta_femininity` and `deaths` diminish in strength?
 # I assume this is because a much larger range of values of `(alpha, beta_femininity)` can produce, through the Gamma distribution, the same expected
-# values `lambda` via the variance in the Gamma process itself
+# values `lambda` via the variance in the Gamma process itself.
+
+## 11H3
+
+# standardize predictors
+d$damage_norm_std <- (d$damage_norm - mean(d$damage_norm)) / sd(d$damage_norm)
+d$femininity_std <- (d$femininity - mean(d$femininity)) / sd(d$femininity)
+d$min_pressure_std <- (d$min_pressure - mean(d$min_pressure)) / sd(d$min_pressure)
+
+# fit a series of models with varying interactions
+model.11H1.single.interaction.damage <- map2stan(
+  alist(
+    deaths ~ dpois( lambda ),
+    log(lambda) <- alpha + beta_femininity_damage_norm*femininity_std*damage_norm_std,
+    alpha ~ dnorm(0, 10),
+    beta_femininity_damage_norm ~ dnorm(0, 1)
+  ),
+  data = d, chains = 2, warmup = 1000, iter = 4000
+)
+
+model.11H1.single.interaction.pressure <- map2stan(
+  alist(
+    deaths ~ dpois( lambda ),
+    log(lambda) <- alpha + beta_femininity_min_pressure*femininity_std*min_pressure_std,
+    alpha ~ dnorm(0, 10),
+    beta_femininity_min_pressure ~ dnorm(0, 1)
+  ),
+  data = d, chains = 2, warmup = 1000, iter = 4000
+)
+
+model.11H1.two.interactions <- map2stan(
+  alist(
+    deaths ~ dpois( lambda ),
+    log(lambda) <- alpha + beta_femininity_min_pressure*femininity_std*min_pressure_std + beta_femininity_damage_norm*femininity_std*damage_norm_std,
+    alpha ~ dnorm(0, 10),
+    c(beta_femininity_min_pressure, beta_femininity_damage_norm) ~ dnorm(0, 10)
+  ),
+  data = d, chains = 2, warmup = 1000, iter = 4000
+)
+
+# compare models
+compare(model.11H1.single.interaction.damage, model.11H1.single.interaction.pressure, model.11H1.two.interactions)
+coeftab(model.11H1.single.interaction.damage, model.11H1.single.interaction.pressure, model.11H1.two.interactions)
+
+# plot counterfactuals
+damage.sequence <- seq(from = -1, to = 6, length.out = 30)
+
+# "masculine" storms
+prediction.data.male <- data.frame(
+  femininity_std = -1,
+  damage_norm_std = damage.sequence
+)
+lambda.male <- link(model.11H1.single.interaction.damage, data = prediction.data.male)
+lambda.male.mu <- apply(X = lambda.male, MARGIN = 2, FUN = mean)
+lambda.male.PI <- apply(X = lambda.male, MARGIN = 2, FUN = PI)
+
+# "feminine" storms
+prediction.data.female <- data.frame(
+  femininity_std = 1,
+  damage_norm_std = damage.sequence
+)
+lambda.female <- link(model.11H1.single.interaction.damage, data = prediction.data.female)
+lambda.female.mu <- apply(X = lambda.female, MARGIN = 2, FUN = mean)
+lambda.female.PI <- apply(X = lambda.female, MARGIN = 2, FUN = PI)
+
+# plot (on square root scale, per author's recommendation)
+plot(d$damage_norm_std, sqrt(d$deaths), pch=ifelse(d$femininity_std>0,16,1), col=rangi2, xlab="damage_norm", ylab="sqrt(deaths)")
+lines( damage.sequence , sqrt(lambda.male.mu) , lty=2 )
+shade( sqrt(lambda.male.PI) , damage.sequence )
+lines( damage.sequence , sqrt(lambda.female.mu) , lty=1 )
+shade( sqrt(lambda.female.PI) , damage.sequence )
