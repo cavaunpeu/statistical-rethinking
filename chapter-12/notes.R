12f0143543f288b1e0dc5456e12285027fead71f
William Wolf <wwolf@shopkeep.com>
2017-03-02 14:49:15 +0000

    add sections 12.21, 12.22, 12.23, 12.24

--- chapter-12/notes.R
+++ chapter-12/notes.R
@@ -126,3 +126,43 @@ partial.pooling.error <- abs(simulated.df$survival.proportion.partial.pooling - 
 ## 12.18
 plot( 1:60, no.pooling.error, xlab="pond", ylab="absolute error", col=rangi2, pch=16 )
 points( 1:60, partial.pooling.error )
+
+## 12.21
+library(rethinking)
+data(chimpanzees)
+d <- chimpanzees
+d$recipient <- NULL # get rid of NAs
+
+m12.4 <- map2stan(
+  alist(
+    pulled_left ~ dbinom(1, p) ,
+    logit(p) <- a + a_actor[actor] + (bp + bpC*condition)*prosoc_left ,
+    a_actor[actor] ~ dnorm(0, sigma_actor),
+    a ~ dnorm(0, 10),
+    bp ~ dnorm(0, 10),
+    bpC ~ dnorm(0, 10),
+    sigma_actor ~ dcauchy(0, 1)
+  ),
+  data=d , warmup=1000 , iter=5000 , chains=4 , cores=3 )
+
+## 12.22
+posterior.samples <- extract.samples(m12.4)
+total_a_actor <- sapply( 1:7 , function(actor) posterior.samples$a + posterior.samples$a_actor[,actor] )
+round(apply(X = total_a_actor, MARGIN = 2, FUN = mean) , 2)
+
+## 12.23
+d$block_id <- d$block
+
+m12.5 <- map2stan(
+  alist(
+    pulled_left ~ dbinom(1, p) ,
+    logit(p) <- a + a_actor[actor] + a_block[block_id] + (bp + bpC*condition)*prosoc_left ,
+    a_actor[actor] ~ dnorm(0, sigma_actor),
+    a_block[block_id] ~ dnorm(0, sigma_block),
+    c(a, bp, bpC) ~ dnorm(0, 10),
+    c(sigma_actor, sigma_block) ~ dcauchy(0, 1)
+  ),
+  data=d , warmup=1000 , iter=5000 , chains=4 , cores=3 )
+
+## 12.24
+precis(m12.5, depth = 2)
