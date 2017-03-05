b0e43cc51ce5129b675137746b2e7f5c212d026f
William Wolf <wwolf@shopkeep.com>
2017-03-05 16:35:59 +0000

    add sections 13.6, 13.7, 13.8, 13.9

--- chapter-13/notes.R
+++ chapter-13/notes.R
@@ -1,5 +1,7 @@
 # notes
 
+library(rethinking)
+
 ## 13.1
 average.morning.wait.time <- 3.5
 average.difference.afternoon.wait.time <- (-1)
@@ -18,3 +20,24 @@ Sigma <- matrix(c(sigma.average.morning.wait.time^2, covariance, covariance, sig
 sigmas <- c(sigma.average.morning.wait.time, sigma.average.difference.afternoon.wait.time)
 Rho <- matrix( c(1, correlation, correlation, 1) , nrow=2 )
 Sigma <- diag(sigmas) %*% Rho %*% diag(sigmas)
+
+## 13.6
+n.cafes <- 20
+
+## 13.7
+library(MASS)
+set.seed(5)
+vary_effects <- mvrnorm( n.cafes , Mu , Sigma )
+
+## 13.8
+a_cafe <- vary_effects[,1]
+b_cafe <- vary_effects[,2]
+
+## 13.9
+plot( a_cafe , b_cafe , col=rangi2 ,
+      xlab="intercepts (a_cafe)" , ylab="slopes (b_cafe)" )
+
+# overlay population distribution
+library(ellipse)
+for ( l in c(0.1, 0.3, 0.5, 0.8, 0.99) )
+  lines(ellipse(Sigma,centre=Mu,level=l) ,col=col.alpha("black",0.2))
