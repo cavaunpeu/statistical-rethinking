c5b11ad812d4775f7bae2d46570bfb8190f6e830
William Wolf <wwolf@shopkeep.com>
2017-03-08 09:55:19 +0000

    add sections 13.12, 13.13, 13.14

--- chapter-13/notes.R
+++ chapter-13/notes.R
@@ -56,3 +56,45 @@ d <- data.frame( cafe=cafe_id , afternoon=afternoon , wait=wait )
 ## 13.11
 R <- rlkjcorr( n = 1e4 , K = 2 , eta = 2 )
 dens( x = R[,1,2] , xlab = "correlation" )
+
+## 13.12
+a <- average.morning.wait.time
+b <- average.difference.afternoon.wait.time
+
+m13.1 <- map2stan(
+  alist(
+    wait ~ dnorm( mu , sigma ),
+    mu <- a_cafe[cafe] + b_cafe[cafe]*afternoon,
+    c(a_cafe, b_cafe)[cafe] ~ dmvnorm2(Mu = c(a, b), sigma = sigma_cafe, Rho = Rho),
+    a ~ dnorm(0, 10),
+    b ~ dnorm(0, 10),
+    sigma_cafe ~ dcauchy(0, 2),
+    sigma ~ dcauchy(0, 2),
+    Rho ~ dlkjcorr(2)
+  ),
+  data=d ,
+  iter=5000 , warmup=2000 , chains=2 )
+
+## 13.13
+posterior.samples <- extract.samples(m13.1)
+dens( posterior.samples$Rho[,1,2] )
+
+## 13.14
+
+# compute unpooled estimates directly from data
+a1 <- sapply( 1:n.cafes ,
+              function(i) mean(wait[cafe_id==i & afternoon==0]) )
+b1 <- sapply( 1:n.cafes ,
+              function(i) mean(wait[cafe_id==i & afternoon==1]) ) - a1
+
+# extract posterior means of partially pooled estimates
+posterior.samples <- extract.samples(m13.1)
+a2 <- apply( X = posterior.samples$a_cafe, MARGIN = 2, FUN = mean )
+b2 <- apply( X = posterior.samples$b_cafe, MARGIN = 2, FUN = mean )
+
+# plot both and connect with lines
+plot( x = a1 , y = b1 , xlab="intercept" , ylab="slope" ,
+      pch=16 , col=rangi2 , ylim=c( min(b1)-0.1 , max(b1)+0.1 ) ,
+      xlim=c( min(a1)-0.1 , max(a1)+0.1 ) )
+points( a2 , b2 , pch=1 )
+for ( i in 1:n.cafes ) lines( c(a1[i],a2[i]) , c(b1[i],b2[i]) )
